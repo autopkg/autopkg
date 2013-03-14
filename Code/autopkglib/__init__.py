9ad9ad016ce83a6d79bd3aec156da2e5a3134149
gregneagle@mac.com <gregneagle@mac.com@fad712a0-fe68-9536-e67b-be8fbbf4d5e8>
2013-03-14 22:33:25 +0000

    Add dynamic import of Processor modules in autopkglib directory and new add_processor function to allow autopkg to dynamically insert Processor modules into autopkglib's namespace.
    git-svn-id: https://macautopkg.googlecode.com/svn/trunk@26 fad712a0-fe68-9536-e67b-be8fbbf4d5e8

--- Code/autopkglib/__init__.py
+++ Code/autopkglib/__init__.py
@@ -14,30 +14,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 
 BUNDLE_ID = "com.googlecode.autopkg"
 
+# get the directory this __init__.py file is in
+mydir = os.path.dirname(os.path.abspath(__file__))
+mydirname = os.path.basename(mydir)
 
-from AdiumURLProvider           import AdiumURLProvider
-from AdobeFlashURLProvider      import AdobeFlashURLProvider
-from AdobeReaderDmgUnpacker     import AdobeReaderDmgUnpacker
-from AdobeReaderURLProvider     import AdobeReaderURLProvider
-from AppDmgVersioner            import AppDmgVersioner
-from Copier                     import Copier
-from CyberduckURLProvider       import CyberduckURLProvider
-from DmgCreator                 import DmgCreator
-from DmgMounter                 import DmgMounter
-from FileCreator                import FileCreator
-from FirefoxURLProvider         import FirefoxURLProvider
-from Firefox36URLProvider       import Firefox36URLProvider
-from AdobeFlashDmgUnpacker      import AdobeFlashDmgUnpacker
-from MunkiInfoCreator           import MunkiInfoCreator
-from PkgCreator                 import PkgCreator
-from PkgInfoCreator             import PkgInfoCreator
-from PkgRootCreator             import PkgRootCreator
-from PraatURLProvider           import PraatURLProvider
-from PraatVersionFixer          import PraatVersionFixer
-from TheUnarchiverURLProvider   import TheUnarchiverURLProvider
-from Unzipper                   import Unzipper
-from URLDownloader              import URLDownloader
-from VLCURLProvider             import VLCURLProvider
+# find all the .py files (minus this one)
+processor_files = [
+        os.path.splitext(name)[0] 
+        for name in os.listdir(mydir) 
+        if name.endswith('.py') and not name == '__init__.py']
+
+# Warning! Fancy dynamic importing ahead!
+#
+# import the filename as a submodule
+# then add the attribute with the same name to the globals()
+#
+# This is the equivalent of:
+#
+#    from Bar.Foo import Foo
+#
+for name in processor_files:
+    globals()[name] = getattr(__import__(
+        mydirname + '.' + name, fromlist=[name]), name)
+
+
+# our fancy dynamic importing replaces the need to do all these
+# explicit imports. More importantly, we can drop additional modules
+# into this package directory and __init__.py will import them
+# automagically
+#from AdiumURLProvider           import AdiumURLProvider
+#from AdobeFlashURLProvider      import AdobeFlashURLProvider
+#from AdobeReaderDmgUnpacker     import AdobeReaderDmgUnpacker
+#from AdobeReaderURLProvider     import AdobeReaderURLProvider
+#from AppDmgVersioner            import AppDmgVersioner
+#from Copier                     import Copier
+#from CyberduckURLProvider       import CyberduckURLProvider
+#from DmgCreator                 import DmgCreator
+#from DmgMounter                 import DmgMounter
+#from FileCreator                import FileCreator
+#from FirefoxURLProvider         import FirefoxURLProvider
+#from Firefox36URLProvider       import Firefox36URLProvider
+#from AdobeFlashDmgUnpacker      import AdobeFlashDmgUnpacker
+#from MunkiInfoCreator           import MunkiInfoCreator
+#from PkgCreator                 import PkgCreator
+#from PkgInfoCreator             import PkgInfoCreator
+#from PkgRootCreator             import PkgRootCreator
+#from PraatURLProvider           import PraatURLProvider
+#from PraatVersionFixer          import PraatVersionFixer
+#from TheUnarchiverURLProvider   import TheUnarchiverURLProvider
+#from Unzipper                   import Unzipper
+#from URLDownloader              import URLDownloader
+#from VLCURLProvider             import VLCURLProvider
+
+def add_processor(processor_name, processor_object):
+    globals()[processor_name] = processor_object
+    
