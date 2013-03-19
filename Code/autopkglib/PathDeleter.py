d7cc4296186b8281989e45d9a4e2f39f1b3b9a04
gregneagle@mac.com <gregneagle@mac.com@fad712a0-fe68-9536-e67b-be8fbbf4d5e8>
2013-03-19 21:17:03 +0000

    Processor that deletes file paths!
    git-svn-id: https://macautopkg.googlecode.com/svn/trunk@30 fad712a0-fe68-9536-e67b-be8fbbf4d5e8

---
+++ Code/autopkglib/PathDeleter.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Greg Neagle
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from Processor import Processor, ProcessorError
+import shutil
+import os
+
+
+__all__ = ["PathDeleter"]
+
+
+class PathDeleter(Processor):
+    description = "Deletes file paths"
+    input_variables = {
+        "path_list": {
+            "required": True,
+            "description": 
+                "List of pathnames to be deleted",
+        },
+    }
+    output_variables = {
+    }
+    
+    __doc__ = description
+    
+    def main(self):
+        for path in self.env["path_list"]:
+            try:
+                if os.path.isfile(path) or os.path.islink(path):
+                    os.remove(path)
+                elif os.path.isdir(path):
+                    shutil.rmtree(path)
+                elif not os.path.exists(path):
+                    raise ProcessorError(
+                        "Could not remove %s - it does not exist!" % path)
+                else:
+                    raise ProcessorError(
+                        "Could not remove %s - it is not a file, link, "
+                        "or directory" % path)
+            except OSError, err:
+                raise ProcessorError(
+                    "Could not remove %s: %s" % (path, err))
+
+
+if __name__ == "__main__":
+    processor = PathDeleter()
+    processor.execute_shell()
