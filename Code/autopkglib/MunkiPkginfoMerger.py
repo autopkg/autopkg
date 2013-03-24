ed8950972520be4ad446ff1be1bee1dda3a1a679
Greg Neagle <gregneagle@mac.com>
2013-03-24 12:31:15 -0700

    Added MunkiPkginfoMerger processor

---
+++ Code/autopkglib/MunkiPkginfoMerger.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Greg Neagle
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from Processor import Processor, ProcessorError
+
+
+__all__ = ["MunkiPkginfoMerger"]
+
+
+class MunkiPkginfoMerger(Processor):
+    description = (
+        "Merges two pkginfo dictionaries.")
+    input_variables = {
+        "pkginfo": {
+            "required": False,
+            "description": "Dictionary of Munki pkginfo.",
+        },
+        "additional_pkginfo": {
+            "required": True,
+            "description": ("Dictionary containing additional Munki pkginfo. "
+                "This will be added to or replace info in the input_pkginfo."),
+        },
+        
+    }
+    output_variables = {
+        "pkginfo": {
+            "description": "Merged pkginfo.",
+        },
+    }
+    
+    __doc__ = description
+    
+    def main(self):
+        if "pkginfo" not in self.env:
+            self.env["pkginfo"] = {}
+            
+        for key in self.env["additional_pkginfo"].keys():
+            self.env["pkginfo"][key] = self.env["additional_pkginfo"][key]
