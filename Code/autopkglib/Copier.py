f63eced0f129e0969d60167ff5436091e088acd5
Timothy Sutton <tim@synthist.net>
2013-10-13 14:11:09 -0400

    Copier.py: support glob for both standard and dmg-mounted 'source_path'
    - log some additional info if globbing is actually doing something

--- Code/autopkglib/Copier.py
+++ Code/autopkglib/Copier.py
@@ -31,8 +31,10 @@ class Copier(DmgMounter):
     input_variables = {
         "source_path": {
             "required": True,
-            "description": "Path to a file or directory to copy. " + \
-                "Can point to a glob path inside a .dmg which will be mounted.",
+            "description": ("Path to a file or directory to copy. ",
+                "Can point to a path inside a .dmg which will be mounted. ",
+                "This path may also contain basic globbing characters such as ",
+                "the wildcard '*', but only the first result will be returned."),
         },
         "destination_path": {
             "required": True,
@@ -75,22 +77,32 @@ class Copier(DmgMounter):
                 "Can't copy %s to %s: %s" % (source_item, dest_item, err))
     
     def main(self):
+        source_path = self.env['source_path']
         # Check if we're trying to copy something inside a dmg.
         (dmg_path, dmg,
-         dmg_source_path) = self.env['source_path'].partition(".dmg/")
+         dmg_source_path) = source_path.partition(".dmg/")
         dmg_path += ".dmg"
         try:
             if dmg:
                 # Mount dmg and copy path inside.
                 mount_point = self.mount(dmg_path)
-                source_path = glob(
-                    os.path.join(mount_point, dmg_source_path))[0]
-            else:
-                # Straight copy from file system.
-                source_path = self.env['source_path']
+                source_path = os.path.join(mount_point, dmg_source_path)
+            # process path with glob.glob
+            matches = glob(source_path)
+            matched_source_path = matches[0]
+            if len(matches) > 1:
+                self.output("WARNING: Multiple paths match 'source_path' glob '%s':"
+                    % source_path)
+                for match in matches:
+                    self.output("  - %s" % match)
+
+            matched_source_path = glob(source_path)[0]
+            if [c for c in '*?[]!' if c in source_path]:
+                self.output("Using path '%s' matched from globbed '%s'."
+                    % (matched_source_path, source_path))
 
             # do the copy
-            self.copy(source_path, self.env['destination_path'],
+            self.copy(matched_source_path, self.env['destination_path'],
                       overwrite=self.env.get("overwrite"))
         finally:
             if dmg:
