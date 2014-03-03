c9c42d8dcfbe40a83acb017bc57785c784c16d7c
Timothy Sutton <tim@synthist.net>
2014-03-03 07:49:01 -0500

    Support version_comparison_key per-installs-item for MunkiInstallsItemsCreator

--- Code/autopkglib/MunkiInstallsItemsCreator.py
+++ Code/autopkglib/MunkiInstallsItemsCreator.py
@@ -19,7 +19,7 @@ import FoundationPlist
 import subprocess
 
 from autopkglib import Processor, ProcessorError
-
+from Foundation import NSDictionary
 
 __all__ = ["MunkiInstallsItemsCreator"]
 
@@ -38,7 +38,15 @@ class MunkiInstallsItemsCreator(Processor):
         },
         "version_comparison_key": {
             "required": False,
-            "description": "Key to use for comparing versions.",
+            "description": ("Set 'version_comparison_key' for installs items. "
+                            "If this is a string, it is set to this value for "
+                            "all items given to 'installs_item_paths'. If this "
+                            "is a dictionary, takes a mapping of a path as "
+                            "given to 'installs_item_paths' to the desired "
+                            "version_comparison_key.\n"
+                            "Example:\n"
+                            "{'/Applications/Foo.app': 'CFBundleVersion',\n"
+                            "'/Library/Bar.plugin': 'CFBundleShortVersionString'}"),
         },
         
     }
@@ -82,18 +90,28 @@ class MunkiInstallsItemsCreator(Processor):
                     item["path"] = item["path"][len(faux_root):]
                 self.output("Created installs item for %s" % item["path"])
 
-        # TODO: should have a way to specify per individual installs item
         if "version_comparison_key" in self.env:
             for item in installs_array:
-                # Check that we really have this key available for comparison
-                if self.env["version_comparison_key"] in item:
-                    item["version_comparison_key"] = self.env["version_comparison_key"]
-                else:
-                    raise ProcessorError(
-                    "version_comparison_key '%s' could not be found in the "
-                    "installs item for path '%s'" % (
-                        self.env["version_comparison_key"],
-                        item["path"]))
+                cmp_key = None
+                # If it's a string, set it for all installs items
+                if isinstance(self.env["version_comparison_key"], basestring):
+                    cmp_key = self.env["version_comparison_key"]
+                # It it's a dict, find if there's a key that matches a path
+                elif isinstance(self.env["version_comparison_key"], NSDictionary):
+                    for path, key in self.env["version_comparison_key"].items():
+                        if path == item["path"]:
+                            cmp_key = key
+
+                if cmp_key:
+                    # Check that we really have this key available for comparison
+                    if cmp_key in item:
+                        item["version_comparison_key"] = cmp_key
+                    else:
+                        raise ProcessorError(
+                        "version_comparison_key '%s' could not be found in the "
+                        "installs item for path '%s'" % (
+                            cmp_key,
+                            item["path"]))
 
         if not "additional_pkginfo" in self.env:
             self.env["additional_pkginfo"] = {}
