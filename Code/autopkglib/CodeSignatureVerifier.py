3afb72d631d52a34a167a62a25e419bdb00c0322
Hannes Juutilainen <hjuutilainen@mac.com>
2014-10-03 22:01:51 +0300

    The codesign --deep option is only available in OS X 10.7 and later

--- Code/autopkglib/CodeSignatureVerifier.py
+++ Code/autopkglib/CodeSignatureVerifier.py
@@ -93,8 +93,12 @@ class CodeSignatureVerifier(DmgMounter):
 
         process = ["/usr/bin/codesign",
                    "--verify",
-                   "--deep",
                    "--verbose=1"]
+
+        # No --deep option in Snow Leopard
+        darwin_version = os.uname()[2]
+        if not darwin_version.startswith("10."):
+            process.append("--deep")
 
         if test_requirement:
             process.append("-R=%s" % test_requirement)
