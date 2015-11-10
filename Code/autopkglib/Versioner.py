59e66101b0b1e792c0bc148e562ccce87e47b627
Shea Craig <sheagcraig@gmail.com>
2015-11-10 15:00:58 -0500

    Refactor AppDmgVersioner into Versioner, with added features.
    - This processor can now handle paths like:
    	- "download.dmg"
    	- "download.dmg/App.app"
    	- "download.dmg/App.app/Contents/Info.plist"
    	- "download.iso" (And any other image type)
    	- "App.app"
    	- "App.app/Contents/Info.plist"
    	- "Plugin.plugin"
    	- "Plugin.plugin/Contents/Info.plist"
    	- "QLGen.qlgenerator"
    	- "QLGen.qlgenerator/Contents/Info.plist"
    - For paths which do not include a bundle (paths to an image type), the
      processor looks for the first bundle it comes across. The search order
      prioritizes apps. This extends the AppDmgVersioner behavior to include
      other types of bundles (although only .app, .plugin, and .qlgenerator
      are currently specified, more can easily be added).
    - Adds the AppDmgVersioner output variables to Versioner (bundleid,
      app_name)
    - Verbose output includes bundle finding results.
    - Exception handling moved to more appropriate places.
    	- Plist parsing errors are handled by get_plist func.
    - try/finally for dmg umounting limited to dmg handling code.

--- Code/autopkglib/Versioner.py
+++ Code/autopkglib/Versioner.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 """See docstring for Versioner class"""
 
+
+import glob
 import os.path
 
 from autopkglib import ProcessorError
@@ -23,6 +25,8 @@ import FoundationPlist
 
 
 __all__ = ["Versioner"]
+BUNDLE_TYPES = (".app", ".plugin", ".qlgenerator")
+BUNDLE_SUFFIX = "Contents/Info.plist"
 
 
 class Versioner(DmgMounter):
@@ -43,7 +47,7 @@ class Versioner(DmgMounter):
             "default": "CFBundleShortVersionString",
             "description":
                 "Which plist key to use; defaults to "
-                 "CFBundleShortVersionString",
+                "CFBundleShortVersionString",
         },
     }
     output_variables = {
@@ -59,34 +63,98 @@ class Versioner(DmgMounter):
     }
 
     def main(self):
-        """Return a version for file at input_plist_path"""
-        # Check if we're trying to read something inside a dmg.
-        (dmg_path, dmg, dmg_source_path) = (
-            self.parsePathForDMG(self.env["input_plist_path"]))
-        try:
-            if dmg:
-                # Mount dmg and copy path inside.
-                mount_point = self.mount(dmg_path)
-                input_plist_path = os.path.join(mount_point, dmg_source_path)
-            else:
-                # just use the given path
-                input_plist_path = self.env["input_plist_path"]
-            if not os.path.exists(input_plist_path):
-                raise ProcessorError(
-                    "File '%s' does not exist or could not be read." %
-                    input_plist_path)
-            try:
-                plist = FoundationPlist.readPlist(input_plist_path)
-                version_key = self.env.get("plist_version_key")
-                self.env["version"] = plist.get(version_key, "UNKNOWN_VERSION")
-                self.output("Found version %s in file %s"
-                            % (self.env["version"], input_plist_path))
-            except FoundationPlist.FoundationPlistException, err:
-                raise ProcessorError(err)
+        """Return an app name, bundleid, and version for file."""
+        input_plist_path = self.env["input_plist_path"]
 
-        finally:
-            if dmg:
-                self.unmount(dmg_path)
+        # Is there an image extension anywhere in the path?
+        if any(ext in input_plist_path for ext in self.DMG_EXTENSIONS):
+            (dmg_path, dmg, _) = (
+                self.parsePathForDMG(input_plist_path))
+
+            if dmg or dmg_path.endswith(tuple(self.DMG_EXTENSIONS)):
+                try:
+                    mount_point = self.mount(dmg_path)
+                    plist_path = self.parse_input_path(mount_point)
+                    plist = self.get_plist(plist_path)
+                finally:
+                    self.unmount(dmg_path)
+
+        # No disk images involved.
+        else:
+            plist_path = self.parse_input_path(input_plist_path)
+            plist = self.get_plist(plist_path)
+
+        self.set_output_variables(plist, plist_path)
+
+    def parse_input_path(self, plist_path):
+        """Ensure a path includes a valid bundle Info.plist.
+
+        If a path does not include a bundle, look for the first bundle
+        at path.
+
+        If a path does not include the suffix Contents/Info.plist, add
+        it.
+        """
+        if not plist_path.endswith(BUNDLE_SUFFIX):
+            if not plist_path.endswith(BUNDLE_TYPES):
+                plist_path = self.find_bundle(plist_path)
+
+            plist_path = os.path.join(plist_path, BUNDLE_SUFFIX)
+        return plist_path
+
+    def find_bundle(self, path):
+        """Find first bundle at path.
+
+        Priority is:
+            .app
+            .plugin
+            .qlgenerator
+
+        Returns:
+            String path to found bundle, or None.
+        """
+        #pylint: disable=no-self-use
+        for bundle_type in BUNDLE_TYPES:
+            bundle = glob.glob(os.path.join(path, "*%s" % bundle_type))
+            if len(bundle):
+                result = bundle[0]
+                self.output("Found bundle %s" % result)
+                return result
+
+    def get_plist(self, plist_path):
+        """Return a plist object from file at plist_path."""
+        #pylint: disable=no-self-use
+        if os.path.exists(plist_path):
+            try:
+                plist = FoundationPlist.readPlist(plist_path)
+            except FoundationPlist.FoundationPlistException as err:
+                raise ProcessorError(err)
+        else:
+            raise ProcessorError(
+                "File '%s' does not exist or could not be read." %
+                plist_path)
+        return plist
+
+    def set_output_variables(self, plist, plist_path):
+        """Set output variables based on plist."""
+        self.env["app_name"] = self.get_app_name(plist_path)
+        self.output("Found app_name %s in file %s" % (
+            self.env["app_name"], plist_path))
+
+        self.env["bundleid"] = plist.get(
+            "CFBundleIdentifier", "UNKNOWN_BUNDLE_ID")
+        self.output("Found bundleid %s in file %s" % (
+            self.env["bundleid"], plist_path))
+
+        version_key = self.env.get("plist_version_key")
+        self.env["version"] = plist.get(version_key, "UNKNOWN_VERSION")
+        self.output("Found version %s in file %s"
+                    % (self.env["version"], plist_path))
+
+    def get_app_name(self, path):
+        """Return the app filename component of a path."""
+        #pylint: disable=no-self-use
+        return os.path.basename(path.split("/" + BUNDLE_SUFFIX)[0])
 
 
 if __name__ == "__main__":
