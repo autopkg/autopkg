f33e53ede399e98977e328fcdec0997a0a1c6a31
Nick McSpadden <nmcspadden@gmail.com>
2019-05-16 10:17:06 -0700

    foundationplist/init with flake8+isort (#511)
    * foundationplist/init with flake8+isort

--- Code/FoundationPlist/__init__.py
+++ Code/FoundationPlist/__init__.py
@@ -1,6 +1,12 @@
+from __future__ import print_function
+
 # We're effectively using this package as module
 try:
     from FoundationPlist import *
 except:
-    print "WARNING: using 'from plistlib import *' instead of 'from FoundationPlist import *' in " + __name__
+    print(
+        "WARNING: using 'from plistlib import *' instead of 'from "
+        "FoundationPlist import *' in " + __name__
+    )
     from plistlib import *
+
