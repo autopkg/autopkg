48ede44a56d462b875012a6768935785570b4f1d
Elliot Jordan <elliot@elliotjordan.com>
2019-12-02 16:01:40 -0800

    Update test_filecreator.py

--- Code/tests/test_filecreator.py
+++ Code/tests/test_filecreator.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/local/autopkg/python
 
+import os
 import plistlib
-import StringIO
 import unittest
+from unittest.mock import patch
 
-import mock
 from autopkglib.FileCreator import FileCreator
+from autopkglib.FileFinder import FileFinder
 
 
 class TestFileCreator(unittest.TestCase):
@@ -14,13 +15,13 @@ class TestFileCreator(unittest.TestCase):
     def setUp(self):
         self.good_env = {"file_content": "Hello world", "file_path": "testfile"}
         self.bad_env = {"file_path": ""}
-        self.input_plist = StringIO.StringIO(plistlib.writePlistToString(self.good_env))
+        self.input_plist = plistlib.dumps(self.good_env)
         self.processor = FileCreator(infile=self.input_plist)
 
     def tearDown(self):
-        self.input_plist.close()
+        pass
 
-    @mock.patch("autopkglib.FileCreator")
+    @patch("autopkglib.FileCreator")
     def test_no_fail_if_good_env(self, _):
         """The processor should not raise any exceptions if run normally."""
         self.processor.env = self.good_env
@@ -28,6 +29,7 @@ class TestFileCreator(unittest.TestCase):
         with open(self.processor.env["file_path"], "r") as openfile:
             test_content = openfile.read()
         self.assertEqual(self.processor.env["file_content"], test_content)
+        os.remove(self.processor.env["file_path"])
 
 
 if __name__ == "__main__":
