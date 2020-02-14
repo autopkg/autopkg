b127ff75fcc60c66660abc9f0027da5244cc1fca
Michal Moravec <michal.moravec@macadmin.cz>
2020-02-14 10:51:54 +0100

    Duplicate mock code to each test method

--- Code/tests/test_versioner.py
+++ Code/tests/test_versioner.py
@@ -53,46 +53,50 @@ class TestVersioner(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @patch("autopkglib.Versioner.load_plist_from_file")
-    @patch("autopkglib.Versioner.parsePathForDMG")
     def run_direct_plist(self, plist, mock_dmg, mock_plist):
+        """Find version in specified plist file."""
         mock_dmg.return_value = (self.processor.env["input_plist_path"], "", "")
         mock_plist.return_value = plistlib.loads(plist)
         self.processor.main()
+
+    @patch("autopkglib.Versioner.load_plist_from_file")
+    @patch("autopkglib.Versioner.parsePathForDMG")
+    def test_no_fail_if_good_env(self, mock_dmg, mock_plist):
+        """The processor should not raise any exceptions if run normally."""
+        self.run_direct_plist(self.info_plist, mock_dmg, mock_plist)
+
+    @patch("autopkglib.Versioner.load_plist_from_file")
+    @patch("autopkglib.Versioner.parsePathForDMG")
+    def test_find_cfbundle_short_version(self, mock_dmg, mock_plist):
+        """The processor should find version in default CFBundleShortVersionString."""
+        self.run_direct_plist(self.info_plist, mock_dmg, mock_plist)
+        self.assertEqual(self.processor.env["version"], self.version_default)
+
+    @patch("autopkglib.Versioner.load_plist_from_file")
+    @patch("autopkglib.Versioner.parsePathForDMG")
+    def test_find_custom_version(self, mock_dmg, mock_plist):
+        """The processor should find version under key specified by plist_version_key."""
+        self.processor.env["plist_version_key"] = self.version_custom_key
+        self.run_direct_plist(self.info_plist, mock_dmg, mock_plist)
+        self.assertEqual(self.processor.env["version"], self.version_custom)
+
+    @patch("autopkglib.Versioner.load_plist_from_file")
+    @patch("autopkglib.Versioner.parsePathForDMG")
+    def test_no_version_found(self, mock_dmg, mock_plist):
+        """The processor should not find version if plist misses it."""
+        self.run_direct_plist(self.no_version_plist, mock_dmg, mock_plist)
+        self.assertEqual(self.processor.env["version"], NO_VERSION_MESSAGE)
 
     @patch("autopkglib.Versioner.unmount")
     @patch("autopkglib.Versioner.mount")
     @patch("autopkglib.Versioner.load_plist_from_file")
     @patch("autopkglib.Versioner.parsePathForDMG")
-    def run_dmg_plist(self, mock_dmg, mock_plist, mock_mount, mock_unmount):
+    def test_no_fail_if_dmg(self, mock_dmg, mock_plist, mock_mount, mock_unmount):
+        """The processor should not raise any exceptions when plist is in the dmg image."""
         mock_dmg.return_value = ("path_to_dmg", ".dmg/", "path_to_plist")
         mock_plist.return_value = plistlib.loads(self.info_plist)
         mock_mount.return_value = "dmg_mount_point"
         self.processor.main()
-
-    def test_no_fail_if_good_env(self):
-        """The processor should not raise any exceptions if run normally."""
-        self.run_direct_plist(self.info_plist)
-
-    def test_find_cfbundle_short_version(self):
-        """The processor should find version in default CFBundleShortVersionString."""
-        self.run_direct_plist(self.info_plist)
-        self.assertEqual(self.processor.env["version"], self.version_default)
-
-    def test_find_custom_version(self):
-        """The processor should find version under key specified by plist_version_key."""
-        self.processor.env["plist_version_key"] = self.version_custom_key
-        self.run_direct_plist(self.info_plist)
-        self.assertEqual(self.processor.env["version"], self.version_custom)
-
-    def test_no_version_found(self):
-        """The processor should not find version if plist misses it."""
-        self.run_direct_plist(self.no_version_plist)
-        self.assertEqual(self.processor.env["version"], NO_VERSION_MESSAGE)
-
-    def test_no_fail_if_dmg(self):
-        """The processor should not raise any exceptions when plist is in the dmg image."""
-        self.run_dmg_plist()
 
 
 if __name__ == "__main__":
