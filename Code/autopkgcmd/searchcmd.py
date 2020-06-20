d87492d54ecb6370f014d55a4a983bfa991a285f
Brian Smith <brian@linuxfood.net>
2020-06-19 17:10:50 -0700

    Create autopkgcmd module to house subcommands
    The main `autopkg` module is quite substantial in size. This makes it                                                                                                          harder to learn how the tool fits together. It also impacts confidence
    that changes have not broken or introduced implicit coupling, increases
    code duplication, makes code review more challenging, and complicates                                                                                                          incrementally adding typing.
    
    In support of moving the `search` command to a dedicated module, a few
    utility functions for github access have been consolidated, and a case
    of duplicated github search queries has been consolidated. The logic for
    initializing or fetching a github token has been refactored to reduce
    code duplication as well. Lastly, more type annotations have been added.
    as well. More typing was added.

---
+++ Code/autopkgcmd/searchcmd.py
@@ -0,0 +1,101 @@
+#!/usr/local/autopkg/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+from urllib.parse import quote
+
+from autopkgcmd.opts import common_parse, gen_common_parser
+from autopkglib import log_err
+from autopkglib.github import (
+    DEFAULT_SEARCH_USER,
+    GitHubSession,
+    print_gh_search_results,
+)
+
+
+def search_recipes(argv: List[str]):
+    """Search recipes on GitHub"""
+    verb = argv[1]
+    parser = gen_common_parser()
+    parser.set_usage(
+        f"Usage: %prog {verb} [options] search_term\n"
+        "Search for recipes on GitHub. The AutoPkg organization "
+        "at github.com/autopkg\n"
+        "is the canonical 'repository' of recipe repos, "
+        "which is what is searched by\n"
+        "default."
+    )
+    parser.add_option(
+        "-u",
+        "--user",
+        default=DEFAULT_SEARCH_USER,
+        help=(
+            "Alternate GitHub user whose repos to search. "
+            f"Defaults to '{DEFAULT_SEARCH_USER}'."
+        ),
+    )
+    parser.add_option(
+        "-p",
+        "--path-only",
+        action="store_true",
+        default=False,
+        help=(
+            "Restrict search results to the recipe's path "
+            "only. Note that the search API currently does not "
+            "support fuzzy matches, so only exact directory or "
+            "filenames (minus the extensions) will be "
+            "returned."
+        ),
+    )
+    parser.add_option(
+        "-t",
+        "--use-token",
+        action="store_true",
+        default=False,
+        help=(
+            "Use a public-scope GitHub token for a higher "
+            "rate limit. If a token doesn't exist, you'll "
+            "be prompted for your account credentials to "
+            "create one."
+        ),
+    )
+
+    # Parse arguments
+    (options, arguments) = common_parse(parser, argv)
+    if len(arguments) < 1:
+        log_err("No search query specified!")
+        return 1
+
+    results_limit = 100
+    term = quote(arguments[0])
+
+    results = GitHubSession().search_for_name(
+        term, options.path_only, options.user, options.use_token, results_limit
+    )
+    if not results:
+        return 2
+
+    print_gh_search_results(results)
+
+    print()
+    print("To add a new recipe repo, use 'autopkg repo-add <repo name>'")
+
+    if len(results) > results_limit:
+        print()
+        print(
+            "Warning: Search yielded more than 100 results. Please try a "
+            "more specific search term."
+        )
+        return 3
+    return 0
---
+++ Code/tests/test_searchcmd.py
@@ -0,0 +1,55 @@
+#!/usr/local/autopkg/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+from unittest.mock import patch
+
+from autopkgcmd import search_recipes
+
+
+class TestSearchCmd(unittest.TestCase):
+    def setUp(self):
+        # Disable preference reading for consistency
+        patch("autopkgcmd.opts.globalPreferences").start()
+        pass
+
+    def test_no_term(self):
+        self.assertEqual(1, search_recipes(["TestSearchCmd", "search"]))
+
+    @patch("autopkgcmd.searchcmd.GitHubSession.code_search")
+    def test_empty_results(self, gh_mock):
+        gh_mock.return_value = []
+        self.assertEqual(
+            2, search_recipes(["TestSearchCmd", "search", "#test-search#"])
+        )
+
+    @patch("autopkgcmd.searchcmd.print_gh_search_results")
+    @patch("autopkgcmd.searchcmd.GitHubSession.search_for_name")
+    def test_too_many_results(self, search_mock, _print_results_mock):
+        search_mock.return_value = list(range(101))
+        self.assertEqual(
+            3, search_recipes(["TestSearchCmd", "search", "#test-search#"])
+        )
+
+    @patch("autopkgcmd.searchcmd.print_gh_search_results")
+    @patch("autopkgcmd.searchcmd.GitHubSession.search_for_name")
+    def test_got_results(self, search_mock, _print_results_mock):
+        search_mock.return_value = list(range(10))
+        self.assertEqual(
+            0, search_recipes(["TestSearchCmd", "search", "#test-search#"])
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
