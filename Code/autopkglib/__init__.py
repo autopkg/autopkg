ea415395767c204dbb2cab951220c742a878d9f8
Brian Smith <brian@linuxfood.net>
2020-06-25 16:43:54 -0700

    Remove unused memoize class.
    It was commented out in all usages. It actualy exists in the standard
    library anyway should it be needed again. And, there was a stale and
    confusing comment involving unit tests that implied fragility that
    didn't exist.

--- Code/autopkglib/__init__.py
+++ Code/autopkglib/__init__.py
@@ -32,34 +32,16 @@ from copy import deepcopy
 from distutils.version import LooseVersion
 
 
-class memoize(dict):
-    """Class to cache the return values of an expensive function.
-    This version supports only functions with non-keyword arguments"""
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args):
-        return self[args]
-
-    def __missing__(self, key):
-        result = self[key] = self.func(*key)
-        return result
-
-
-# @memoize
 def is_mac():
     """Return True if current OS is macOS."""
     return "Darwin" in platform.platform()
 
 
-# @memoize
 def is_windows():
     """Return True if current OS is Windows."""
     return "Windows" in platform.platform()
 
 
-# @memoize
 def is_linux():
     """Return True if current OS is Linux."""
     return "Linux" in platform.platform()
