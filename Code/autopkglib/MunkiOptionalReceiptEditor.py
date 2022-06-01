a3a743ad8d1f482ada02c16ce683ff630f493013
Nick McSpadden <nmcspadden@gmail.com>
2022-06-01 10:30:51 -0700

    Added MunkiOptionalReceiptEditor

---
+++ Code/autopkglib/MunkiOptionalReceiptEditor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# This was originally written by Sam Keeley:
+# https://github.com/autopkg/keeleysam-recipes/blob/master/GoogleTalkPlugin/MunkiPkginfoReceiptsEditor.py
+# It has been modified and reformatted.
+
+from autopkglib import Processor, ProcessorError
+import plistlib
+
+__all__ = ["MunkiOptionalReceiptEditor"]
+
+
+class MunkiOptionalReceiptEditor(Processor):
+    """Modifies the receipts key in a Munki pkginfo."""
+
+    input_variables = {
+        "pkginfo_repo_path": {
+            "required": True,
+            "description": "The repo path where the pkginfo was written.",
+        },
+        "pkg_ids_set_optional_true": {
+            "required": True,
+            "description": "Array of package IDs to turn optional for Munki",
+        },
+    }
+    output_variables = {}
+
+    description = __doc__
+
+    def main(self):
+        if len(self.env["pkginfo_repo_path"]) < 1:
+            raise ProcessorError("You must specify at least one pkginfo repo path")
+
+        with open(self.env["pkginfo_repo_path"], "rb") as f:
+            pkginfo = plistlib.load(f)
+
+        receipts_modified = []
+        if "receipts" in pkginfo.keys():
+            for i, receipt in enumerate(pkginfo["receipts"]):
+                # made optional any pkginfos
+                if receipt["packageid"] in self.env["pkg_ids_set_optional_true"]:
+                    pkginfo["receipts"][i]["optional"] = True
+                    self.output(
+                        f"Setting package ID {receipt['packageid']} as optional"
+                    )
+                    receipts_modified.append(receipt["packageid"])
+        else:
+            raise ProcessorError("pkginfo does not contain any receipts")
+
+        if len(receipts_modified) > 0:
+            self.output(f"Writing pkginfo to {self.env['pkginfo_repo_path']}")
+            with open(self.env["pkginfo_repo_path"], "wb") as f:
+                plistlib.dump(pkginfo, f)
+        else:
+            self.output("No receipts modified, nothing to do")
+
+
+if __name__ == "__main__":
+    processor = MunkiOptionalReceiptEditor()
+    processor.execute_shell()
