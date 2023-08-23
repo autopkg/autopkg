a113d5a17d723b98a739e1a21451852fd38507b4
Nick McSpadden <nmcspadden@gmail.com>
2023-08-23 13:49:44 -0700

    Added python package script

---
+++ Scripts/make_autopkg_python_pkg.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# This is intended to be run directly from the Scripts directory.
+
+PYVERSION=$(/usr/libexec/PlistBuddy -c "Print:PythonVersion" "../Code/autopkglib/version.plist")
+WORKROOT=$(mktemp -d /tmp/AutoPkg-build-root-XXXXXXXXXXX)
+git clone https://github.com/gregneagle/relocatable-python.git "$WORKROOT/relocatable-python"
+PKGROOT="$WORKROOT/Library/AutoPkg/Python3"
+mkdir -p "$PKGROOT"
+/usr/bin/python3 "$WORKROOT/relocatable-python/make_relocatable_python_framework.py" \
+    --python-version "$PYVERSION" \
+    --pip-requirements "../new_requirements.txt" \
+    --os-version "11" \
+    --destination "$PKGROOT/Python.framework"
+
+pkgbuild --root "$PKGROOT" \
+--identifier com.github.autopkg.python \
+--version "$PYVERSION" \
+"artifacts/AutoPkg-Python-$PYVERSION.pkg"
