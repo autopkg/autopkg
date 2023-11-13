e29d0559dbab7bc29762e6a7a1f46a09e4844aa5
Carlos Hernandez <carlos@hrndz.ca>
2023-11-12 22:32:34 -0700

    Update tests to be compatible with py311
    see:
    https://github.com/scikit-hep/pyhf/issues/2143#issuecomment-1647467653

--- tests/test_urltextsearcher.py
+++ tests/test_urltextsearcher.py
@@ -1,12 +1,15 @@
 #!/usr/local/autopkg/python
 
+import importlib
 import plistlib
 import unittest
 from textwrap import dedent
 from unittest.mock import patch
 
 from autopkg.autopkglib import ProcessorError
-from autopkg.autopkglib.URLTextSearcher import NO_MATCH_MESSAGE, URLTextSearcher
+from autopkg.autopkglib.URLTextSearcher import NO_MATCH_MESSAGE
+
+URLTextSearcher = importlib.import_module("autopkg.autopkglib.URLTextSearcher")
 
 
 class TestURLTextSearcher(unittest.TestCase):
@@ -41,7 +44,7 @@ class TestURLTextSearcher(unittest.TestCase):
             f"{self.match['protocol']}://{self.match['server']}/FiRsT.dmg"
         )
         self.input_plist = plistlib.dumps(self.good_env)
-        self.processor = URLTextSearcher(infile=self.input_plist)
+        self.processor = URLTextSearcher.URLTextSearcher(infile=self.input_plist)
         self.processor.env = self.good_env
 
     def tearDown(self):
@@ -60,43 +63,43 @@ class TestURLTextSearcher(unittest.TestCase):
             self.processor.main()
         self.assertTrue(NO_MATCH_MESSAGE in str(err.exception))
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_no_fail_if_good_env(self, mock_download):
         """The processor should not raise any exceptions if run normally."""
         mock_download.return_value = self.web_page
         self.processor.main()
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_found_a_match(self, mock_download):
         """If processor finds a match, it should be in the env."""
         self.match_first(mock_download)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_not_found_a_match(self, mock_download):
         """If processor does not find a match, ProcessorError should be raised."""
         self.processor.env["re_pattern"] = "https://badpattern.pkg"
         self.nomatch_exception(mock_download)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_case_sensitive_re_pattern_default(self, mock_download):
         """Difference in character case should not produce a match by default."""
         self.processor.env["re_pattern"] = self.case_sensitive_pattern
         self.nomatch_exception(mock_download)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_case_insensitive_re_pattern_flag(self, mock_download):
         """With re.IGNORECASE flag difference in character case should match."""
         self.processor.env["re_pattern"] = self.case_sensitive_pattern
         self.processor.env["re_flags"] = ["IGNORECASE"]
         self.match_first(mock_download)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_match_last_unnamed_group(self, mock_download):
         """Only last unnamed re group should matchwhen multiple groups used."""
         self.processor.env["re_pattern"] = f'(<a href=")({self.first_match})"'
         self.match_first(mock_download)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_match_named_group_with_output_var(self, mock_download):
         """Only named group with name equal to result_output_var_name should match."""
         output_var_name = "my_match"
@@ -106,7 +109,7 @@ class TestURLTextSearcher(unittest.TestCase):
         ] = f'(<a href=")(?P<{output_var_name}>{self.first_match})(")'
         self.match_first(mock_download, result_output_var_name=output_var_name)
 
-    @patch("autopkg.autopkglib.URLTextSearcher.download_with_curl")
+    @patch(f"{__name__}.URLTextSearcher.URLTextSearcher.download_with_curl")
     def test_match_multiple_named_groups(self, mock_download):
         """Processor should return output variable for each named re group."""
         self.processor.env[
