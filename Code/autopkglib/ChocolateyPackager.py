e7b1de27f7911bdceb644b325286dbfca0ea83b6
Elliot Jordan <elliot@elliotjordan.com>
2025-07-04 13:45:09 -0700

    Fix processor reference in docstring

--- Code/autopkglib/ChocolateyPackager.py
+++ Code/autopkglib/ChocolateyPackager.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""See docstring for NugetChocoPackager class"""
+"""See docstring for ChocolateyPackager class"""
 
 import os
 import subprocess
