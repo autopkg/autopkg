autopkg/autopkg

commit 366ad8c20458b045746b6a67bf8e20d54dc61ad2

Make defaults explicit that were previously only implied or described

Code/autopkglib/AppPkgCreator.py
@@ -40,11 +40,13 @@ class AppPkgCreator(DmgMounter, PkgCreator):
                 "Typically %pathname% points to a disk image downloaded in a "
                 "prior recipe step."
             ),
+            "default": "%pathname%/*.app",
         },
         "pkg_path": {
             "required": False,
             "description": "The pathname for the pkg to be created. If not set, "
             "defaults to %RECIPE_CACHE_DIR%/%app_name%-%version%.pkg",
+            "default": "%RECIPE_CACHE_DIR%/%app_name%-%version%.pkg",
         },
         "bundleid": {
             "required": False,
@@ -61,6 +63,7 @@ class AppPkgCreator(DmgMounter, PkgCreator):
             "description": "Alternate key from which to get the app version. "
             "If the key does not exist in the app's Info.plist, a "
             "ProcessorError will be raised.",
+            "default": "CFBundleShortVersionString",
         },
         "force_pkg_build": {
             "required": False,
@@ -69,6 +72,7 @@ class AppPkgCreator(DmgMounter, PkgCreator):
                 "a package already exists in the output directory with "
                 "the same identifier and version number. Defaults to False"
             ),
+            "default": False,
         },
     }
     output_variables = {

Code/autopkglib/CodeSignatureVerifier.py
@@ -79,6 +79,7 @@ class CodeSignatureVerifier(DmgMounter):
                 "recursively verified as to its full content. Note that this option "
                 "is ignored if the current system version is less than 10.9."
             ),
+            "default": True,
         },
         "strict_verification": {
             "required": False,

Code/autopkglib/DmgCreator.py
@@ -40,12 +40,14 @@ class DmgCreator(Processor):
         "dmg_format": {
             "required": False,
             "description": (f"The dmg format. Defaults to {DEFAULT_DMG_FORMAT}."),
+            "default": DEFAULT_DMG_FORMAT,
         },
         "dmg_filesystem": {
             "required": False,
             "description": (
                 f"The dmg filesystem. Defaults to {DEFAULT_DMG_FILESYSTEM}."
             ),
+            "default": DEFAULT_DMG_FILESYSTEM,
         },
         "dmg_zlib_level": {
             "required": False,
@@ -55,6 +57,7 @@ class DmgCreator(Processor):
                 "beyond which very little space savings is "
                 "gained."
             ),
+            "default": DEFAULT_ZLIB_LEVEL,
         },
         "dmg_megabytes": {
             "required": False,

Code/autopkglib/FlatPkgUnpacker.py
@@ -51,6 +51,7 @@ class FlatPkgUnpacker(DmgMounter):
                 "This means components of the package will not be "
                 "extracted such as scripts."
             ),
+            "default": False,
         },
         "destination_path": {
             "required": True,

Code/autopkglib/MunkiImporter.py
@@ -121,6 +121,7 @@ class MunkiImporter(Processor):
         "MUNKI_PKGINFO_FILE_EXTENSION": {
             "required": False,
             "description": "Extension for output pkginfo files. Default is 'plist'.",
+            "default": "plist",
         },
         "metadata_additions": {
             "required": False,

Code/autopkglib/PkgCreator.py
@@ -48,6 +48,7 @@ class PkgCreator(Processor):
                 "a package already exists in the output directory with "
                 "the same identifier and version number. Defaults to False"
             ),
+            "default": False,
         },
     }
     output_variables = {

Code/autopkglib/SparkleUpdateInfoProvider.py
@@ -59,6 +59,7 @@ class SparkleUpdateInfoProvider(URLGetter):
                 "appcast is using an alternate one. Defaults to "
                 "that used for 'vanilla' Sparkle appcasts."
             ),
+            "default": DEFAULT_XMLNS,
         },
         "curl_opts": {
             "required": False,
@@ -89,6 +90,7 @@ class SparkleUpdateInfoProvider(URLGetter):
                 "from the sparkle feed needs to be urlencoded. "
                 "Defaults to True."
             ),
+            "default": True,
         },
         "update_channel": {
             "required": False,

commit ca4a27ad74f31be3fc937ae38360a19c9dc93ada

Remove explicit defaults that require variable substitution
Fixes bug introduced in 366ad8c.

Code/autopkglib/AppPkgCreator.py
@@ -41,13 +41,11 @@ class AppPkgCreator(DmgMounter, PkgCreator):
                 "Typically %pathname% points to a disk image downloaded in a "
                 "prior recipe step."
             ),
-            "default": "%pathname%/*.app",
         },
         "pkg_path": {
             "required": False,
             "description": "The pathname for the pkg to be created. If not set, "
             "defaults to %RECIPE_CACHE_DIR%/%app_name%-%version%.pkg",
-            "default": "%RECIPE_CACHE_DIR%/%app_name%-%version%.pkg",
         },
         "bundleid": {
             "required": False,