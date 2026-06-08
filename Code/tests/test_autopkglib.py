autopkg/autopkg

commit 8d169522d111468110e680e66e34230457301caa

Coerce substitution values to str in getdata
Co-authored-by: JGStew <james@jgstew.com>

Code/autopkglib/__init__.py
@@ -1124,9 +1124,9 @@ def update_data(a_dict, key, value) -> None:
     """Update a_dict keys with value. Existing data can be referenced
     by wrapping the key in %percent% signs."""
 
-    def getdata(match) -> Any:
-        """Returns data from a match object"""
-        return a_dict[match.group("key")]
+    def getdata(match) -> str:
+        """Returns data from a match object, coerced to str for substitution."""
+        return str(a_dict[match.group("key")])
 
     def do_variable_substitution(item) -> Any:
         """Do variable substitution for item"""

commit bda037c84ac7c1cd4360686447ae85b5003b5cd3

Fix null substitutions represented as strings
Regression introduced in 8d16952

Code/autopkglib/__init__.py
@@ -1202,7 +1202,8 @@ def update_data(a_dict, key, value) -> None:
 
     def getdata(match) -> str:
         """Returns data from a match object, coerced to str for substitution."""
-        return str(a_dict[match.group("key")])
+        val = a_dict[match.group("key")]
+        return "" if val is None else str(val)
 
     def do_variable_substitution(item) -> Any:
         """Do variable substitution for item"""

Code/tests/test_autopkglib.py
@@ -484,6 +484,12 @@ class TestUpdateData(unittest.TestCase):
         autopkglib.update_data(env, "NAME", env["NAME"])
         self.assertEqual(env["NAME"], "App-1.5")
 
+    def test_none_value_substituted_as_empty_string(self):
+        """None/null values referenced via %KEY% must expand to empty string."""
+        env = {"INCLUDE_PRERELEASES": None, "ARG": "%INCLUDE_PRERELEASES%"}
+        autopkglib.update_data(env, "ARG", env["ARG"])
+        self.assertEqual(env["ARG"], "")
+
     def test_plain_string_substitution_unchanged(self):
         """String values are substituted as-is."""
         env = {"NAME": "Firefox", "PATH": "%NAME%.pkg"}