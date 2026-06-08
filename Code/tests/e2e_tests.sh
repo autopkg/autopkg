2eedc94978d06d75cc04dd6900504e9070575a21
Elliot Jordan <homebysix@users.noreply.github.com>
2026-06-08 08:44:23 -0700

    Update e2e verb test shell script

--- Code/tests/e2e_tests.sh
+++ Code/tests/e2e_tests.sh
@@ -1,40 +1,82 @@
 #!/bin/bash
+# End-to-end integration tests for AutoPkg CLI verbs. Exercises each verb
+# against a real git repo clone in an isolated temp directory. macOS only.
+
+TESTDIR="${TMPDIR%/}/autopkg-e2e-tests"
+AUTOPKG=../Code/autopkg
+
+rm -rf "$TESTDIR"
+mkdir -p "$TESTDIR/Cache" "$TESTDIR/RecipeRepos" "$TESTDIR/RecipeOverrides" "$TESTDIR/munki_repo"
+trap 'rm -rf "$TESTDIR"' EXIT
+
+PREFS_FILE="$TESTDIR/preferences.plist"
+cat > "$PREFS_FILE" << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CACHE_DIR</key>
+	<string>$TESTDIR/Cache</string>
+	<key>MUNKI_REPO</key>
+	<string>$TESTDIR/munki_repo</string>
+	<key>RECIPE_MAP_PATH</key>
+	<string>$TESTDIR/recipe_map.json</string>
+	<key>RECIPE_OVERRIDE_DIRS</key>
+	<string>$TESTDIR/RecipeOverrides</string>
+	<key>RECIPE_REPOS</key>
+	<dict/>
+	<key>RECIPE_REPO_DIR</key>
+	<string>$TESTDIR/RecipeRepos</string>
+	<key>RECIPE_SEARCH_DIRS</key>
+	<array>
+		<string>.</string>
+	</array>
+</dict>
+</plist>
+EOF
+PREFS="--prefs $PREFS_FILE"
 
 echo "**Help:"
-../Code/autopkg help
+$AUTOPKG help
 echo "**List-processors:"
-../Code/autopkg list-processors --prefs tests/preferences.plist
+$AUTOPKG list-processors $PREFS
 echo "**Processor-info:"
-../Code/autopkg processor-info URLDownloader --prefs tests/preferences.plist
+$AUTOPKG processor-info URLDownloader $PREFS
 echo "**Repo-add:"
-../Code/autopkg repo-add recipes --prefs tests/preferences.plist
+$AUTOPKG repo-add recipes $PREFS
 echo "**Repo-list:"
-../Code/autopkg repo-list --prefs tests/preferences.plist
+$AUTOPKG repo-list $PREFS
 echo "**Repo-update:"
-../Code/autopkg repo-update all --prefs tests/preferences.plist
+$AUTOPKG repo-update all $PREFS
+echo "**Generate-recipe-map:"
+$AUTOPKG generate-recipe-map $PREFS
 echo "**Audit:"
-../Code/autopkg audit Firefox.munki --prefs tests/preferences.plist
+$AUTOPKG audit Firefox.munki $PREFS
 echo "**Info:"
-../Code/autopkg info Firefox.munki --prefs tests/preferences.plist
+$AUTOPKG info Firefox.munki $PREFS
 echo "**List-recipes:"
-../Code/autopkg list-recipes --prefs tests/preferences.plist
+$AUTOPKG list-recipes $PREFS
 echo "**Make-override:"
-../Code/autopkg make-override Firefox.munki --force --prefs tests/preferences.plist
+$AUTOPKG make-override Firefox.munki --force $PREFS
 echo "**New-recipe:"
-../Code/autopkg new-recipe TestRecipe.check --prefs tests/preferences.plist
+$AUTOPKG new-recipe "$TESTDIR/TestRecipe.check" $PREFS
 echo "**Search:"
-../Code/autopkg search Firefox --prefs tests/preferences.plist
+$AUTOPKG search Firefox $PREFS
 echo "**Verify-trust-info:"
-../Code/autopkg verify-trust-info Firefox.munki --prefs tests/preferences.plist
+$AUTOPKG verify-trust-info Firefox.munki $PREFS
 echo "**Update-trust-info:"
-../Code/autopkg update-trust-info Firefox.munki --prefs tests/preferences.plist
+$AUTOPKG update-trust-info Firefox.munki $PREFS
 echo "**Version:"
-../Code/autopkg version
+$AUTOPKG version
 echo "**Run:"
-../Code/autopkg run -vv Firefox.munki --prefs tests/preferences.plist
+$AUTOPKG run --check -vv Firefox.munki $PREFS
+echo "**Clear-cache (recipe):"
+$AUTOPKG clear-cache Firefox.munki $PREFS
 echo "**Run many:"
-../Code/autopkg run -vv Firefox.munki AdobeFlashPlayer.munki MakeCatalogs.munki --prefs tests/preferences.plist
+$AUTOPKG run --check -vv Firefox.munki AdobeFlashPlayer.munki MakeCatalogs.munki $PREFS
+echo "**Clear-cache (all):"
+$AUTOPKG clear-cache all $PREFS
 echo "**Install:"
-../Code/autopkg install Firefox -vv --prefs tests/preferences.plist
+$AUTOPKG install VLC -vv $PREFS
 echo "**Repo-delete:"
-../Code/autopkg repo-delete recipes --prefs tests/preferences.plist
+$AUTOPKG repo-delete recipes $PREFS
